024700f646d697cef26cc8673c2c48229bcdc4cd
言志伟 <yanzhiwei@kezaihui.com>
2018-11-15 16:06:03 +0800

    [Common]增加ObjectHelper和CollectionHelper辅助类以及单元测试

---
+++ MasterChief.DotNet4.UtilitiesTests/Common/CollectionHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace MasterChief.DotNet4.Utilities.Common.Tests
+{
+    [TestClass()]
+    public class CollectionHelperTests
+    {
+        [TestMethod()]
+        public void IsNullOrEmptyTest()
+        {
+            List<int> data1 = new List<int> { };
+            Assert.IsTrue(data1.IsNullOrEmpty());
+
+            List<int> data2 = new List<int> { 1, 2, 3 };
+            Assert.IsFalse(data2.IsNullOrEmpty());
+
+            string[] data3 = { "123", "456" };
+            Assert.IsFalse(data3.IsNullOrEmpty());
+        }
+    }
+}
---
+++ MasterChief.DotNet4.UtilitiesTests/Common/ObjectHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MasterChief.DotNet4.Utilities.Common.Tests
+{
+    [TestClass()]
+    public class ObjectHelperTests
+    {
+        [TestMethod()]
+        public void IsCollectionTest()
+        {
+            object data1 = null;
+            Assert.IsFalse(data1.IsCollection());
+            int data2 = 123;
+            Assert.IsFalse(data2.IsCollection());
+            string[] data3 = { "1", "2", "3" };
+            Assert.IsTrue(data3.IsCollection());
+            List<int> data4 = new List<int>() { 1, 3, 4 };
+            Assert.IsTrue(data4.IsCollection());
+
+            ArrayList data5 = new ArrayList();
+            Assert.IsTrue(data5.IsCollection());
+
+            HashSet<int> data6 = new HashSet<int>();
+            Assert.IsTrue(data6.IsCollection());
+        }
+    }
+}
