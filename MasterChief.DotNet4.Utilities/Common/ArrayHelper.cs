9f133a7ee9bca26e8c4a86f35510668814dc4730
言志伟 <yanzhiwei@kezaihui.com>
2018-11-15 17:26:38 +0800

    增加辅助类

---
+++ MasterChief.DotNet4.Utilities/Common/ArrayHelper.cs
@@ -0,0 +1,58 @@
+﻿namespace MasterChief.DotNet4.Utilities.Common
+{
+    using System;
+
+    /// <summary>
+    /// Array 辅助类
+    /// </summary>
+    public static class ArrayHelper
+    {
+        #region Methods
+
+        /// <summary>
+        /// 向数组添加一个元素
+        /// </summary>
+        /// <typeparam name="T">数组类型</typeparam>
+        /// <param name="source">原始数组</param>
+        /// <param name="item">元素</param>
+        /// <returns>新的数组</returns>
+        public static T[] Add<T>(this T[] source, T item)
+        {
+            int count = source.Length;
+            Array.Resize<T>(ref source, count + 1);
+            source[count] = item;
+            return source;
+        }
+
+        /// <summary>
+        /// 像数组添加数组
+        /// </summary>
+        /// <typeparam name="T">数组类型</typeparam>
+        /// <param name="source">原始数组</param>
+        /// <param name="target">添加数组</param>
+        /// <returns>新的数组</returns>
+        public static T[] AddRange<T>(this T[] source, T[] target)
+        {
+            int count = source.Length;
+            int targetCount = target.Length;
+            Array.Resize<T>(ref source, count + targetCount);
+            target.CopyTo(source, count);
+            return source;
+        }
+
+        /// <summary>
+        /// 判断数组是否是空还是NULL
+        /// </summary>
+        /// <param name="source">原始数组</param>
+        public static bool IsNullOrEmpty(this Array source)
+        {
+            if (source == null || source.Length == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ MasterChief.DotNet4.UtilitiesTests/Common/ArrayHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MasterChief.DotNet4.Utilities.Common.Tests
+{
+    [TestClass()]
+    public class ArrayHelperTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            string[] data = { "a", "b", "c" };
+            string[] actual = data.Add("d");
+            string[] expect = { "a", "b", "c", "d" };
+            CollectionAssert.AreEqual(expect, actual);
+        }
+
+        [TestMethod()]
+        public void AddRangeTest()
+        {
+            string[] data = { "a", "b", "c" };
+            string[] actual = data.AddRange(new string[] { "d", "e" });
+            string[] expect = { "a", "b", "c", "d", "e" };
+            CollectionAssert.AreEqual(expect, actual);
+        }
+    }
+}
