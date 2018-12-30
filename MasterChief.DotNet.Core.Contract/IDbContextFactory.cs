27b63cbfc44e7b44367544b557272ff199618859
YanZhiwei <Yan.Zhiwei@hotmail.com>
2018-12-31 02:04:45 +0800

    coding

--- MasterChief.DotNet.Core.Contract/IDbContextFactory.cs
+++ MasterChief.DotNet.Core.Contract/IDbContextFactory.cs
@@ -1,7 +1,14 @@
 ﻿namespace MasterChief.DotNet.Core.Contract
 {
+    /// <summary>
+    /// Db context factory.
+    /// </summary>
     public interface IDbContextFactory
     {
+        /// <summary>
+        /// Create this instance.
+        /// </summary>
+        /// <returns>The create.</returns>
         IDbContext Create();
     }
 }
