8a429dcd4b6b8efb1b4a51b9ffcbef3850e161c4
言志伟 <yanzhiwei@kezaihui.com>
2018-12-31 14:51:53 +0800

    coding

---
+++ MasterChief.DotNet.Core.Log/AutoDeleteFileAppender.cs
@@ -0,0 +1,106 @@
+﻿namespace MasterChief.DotNet.Core.Log
+{
+    using log4net.Appender;
+    using log4net.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// 自动删除多少天日志 RollingFileAppender
+    /// </summary>
+    /// <seealso cref="log4net.Appender.RollingFileAppender" />
+    public class AutoDeleteFileAppender : RollingFileAppender
+    {
+        #region Fields
+
+        private static readonly Type _declaringType = typeof(AutoDeleteFileAppender);
+
+        private string _baseDirectory = string.Empty;
+
+        #endregion Fields
+
+        #region Properties
+
+        /// <summary>
+        /// 最多保留多少天日志
+        /// </summary>
+        public int MaxNumberOfDays
+        {
+            get; set;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Performs any required rolling before outputting the next event
+        /// </summary>
+        /// <remarks>
+        /// Handles append time behavior for RollingFileAppender.  This checks
+        /// if a roll over either by date (checked first) or time (checked second)
+        /// is need and then appends to the file last.
+        /// </remarks>
+        protected override void AdjustFileBeforeAppend()
+        {
+            DateTime now = DateTime.Now;
+            int curHour = now.Hour, curMinu = now.Minute, curSec = now.Second;
+
+            if (curHour == 23 && curMinu == 59 && curSec == 59)//每天执行一次
+            {
+                if (MaxNumberOfDays > 0 && Directory.Exists(_baseDirectory))
+                {
+                    RemoveOldLogFiles();
+                }
+            }
+
+            base.AdjustFileBeforeAppend();
+        }
+
+        /// <summary>
+        /// Creates and opens the file for logging.  If <see cref="P:log4net.Appender.RollingFileAppender.StaticLogFileName" />
+        /// is false then the fully qualified name is determined and used.
+        /// </summary>
+        /// <param name="fileName">the name of the file to open</param>
+        /// <param name="append">true to append to existing file</param>
+        /// <remarks>
+        /// This method will ensure that the directory structure
+        /// for the <paramref name="fileName" /> specified exists.
+        /// </remarks>
+        protected override void OpenFile(string fileName, bool append)
+        {
+            _baseDirectory = Path.GetDirectoryName(fileName);
+            string fileNameOnly = Path.GetFileName(fileName);
+
+            base.OpenFile(fileName, append);
+        }
+
+        protected void RemoveOldLogFiles()
+        {
+            try
+            {
+                string[] logFiles = Directory.GetFiles(_baseDirectory, "*.log", SearchOption.AllDirectories);
+                List<FileInfo> fiLogList = new List<FileInfo>();
+                foreach (string file in logFiles)
+                {
+                    fiLogList.Add(new FileInfo(file));
+                }
+
+                foreach (FileInfo item in fiLogList)
+                {
+                    if (item.LastAccessTime < DateTime.Now.AddDays(-MaxNumberOfDays))
+                    {
+                        base.DeleteFile(item.FullName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.Error(string.Format("删除{0}天日志发生错误", MaxNumberOfDays), ex, ErrorCode.GenericFailure);
+            }
+        }
+
+        #endregion Methods
+    }
+}
