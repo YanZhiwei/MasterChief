ec6dfe4d5c6f6ef7edb315bfd19128e0da70568a
YanZhiwei <Yan.Zhiwei@outlook.com>
2019-03-09 22:57:43 +0800

    调整dapper 对应.net版本

--- MasterChief.DotNet.Core.EF/Properties/AssemblyInfo.cs
+++ MasterChief.DotNet.Core.EF/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // 有关程序集的一般信息由以下
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MasterChief.DotNet.Core.EF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MasterChief.DotNet.Core.EF")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("MeetYan@outlook.com")]
+[assembly: AssemblyProduct("MasterChief.DotNet.Core.Contract")]
+[assembly: AssemblyCopyright("Copyright © YanZhiwei 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,7 +21,7 @@ using System.Runtime.InteropServices;
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("483b0077-53f1-4f79-a802-596998f19b2b")]
 
-// 程序集的版本信息由下列四个值组成: 
+// 程序集的版本信息由下列四个值组成:
 //
 //      主版本
 //      次版本
