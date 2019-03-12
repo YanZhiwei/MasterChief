4eb5eb823b920af46fbe2ce9a05c6988a3969113
言志伟 <yanzhiwei@kezaihui.com>
2019-03-12 14:50:13 +0800

    增加winform 双向绑定

---
+++ MasterChief.DotNet4.Utilities/Common/ExpressionHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace MasterChief.DotNet4.Utilities.Common
+{
+    /// <summary>
+    ///Expression 辅助类
+    /// </summary>
+    public static class ExpressionHelper
+    {
+        /// <summary>
+        /// 获取Member
+        /// </summary>
+        /// <param name="expression">Expression</param>
+        /// <returns>名称</returns>
+        public static string GetMemberName(this Expression expression)
+        {
+            switch (expression.NodeType)
+            {
+                case ExpressionType.MemberAccess:
+                    MemberExpression memberExpression = (MemberExpression)expression;
+                    string supername = GetMemberName(memberExpression.Expression);
+                    if (string.IsNullOrEmpty(supername))
+                    {
+                        return memberExpression.Member.Name;
+                    }
+
+                    return string.Concat(supername, '.', memberExpression.Member.Name);
+
+                case ExpressionType.Call:
+                    MethodCallExpression callExpression = (MethodCallExpression)expression;
+                    return callExpression.Method.Name;
+
+                case ExpressionType.Convert:
+                    UnaryExpression unaryExpression = (UnaryExpression)expression;
+                    return GetMemberName(unaryExpression.Operand);
+
+                case ExpressionType.Parameter:
+                case ExpressionType.Constant:
+                    return string.Empty;
+
+                default:
+                    throw new ArgumentException("The expression is not a member access or method call expression");
+            }
+        }
+    }
+}
---
+++ MasterChief.DotNet4.Utilities/ComponentModel/NotificationObject.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace MasterChief.DotNet4.Utilities.ComponentModel
+{
+    /// <summary>
+    ///通知界面属性变更辅助类
+    /// </summary>
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        #region Events
+
+        /// <summary>
+        /// 属性通知事件
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
+
+        #region Methods
+
+        /// <summary>
+        /// 通知
+        /// </summary>
+        /// <param name="propertyName">属性名称</param>
+        public void NotifyChanges(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion Methods
+    }
+}
