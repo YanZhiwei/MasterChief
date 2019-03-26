86819da5ed71748b1a915f6e0dc60832e07c628a
YanZhiwei <Yan.Zhiwei@outlook.com>
2019-03-27 01:09:03 +0800

    完善nuget

--- MasterChief.DotNet.Infrastructure.VerifyCode/Properties/AssemblyInfo.cs
+++ MasterChief.DotNet.Infrastructure.VerifyCode/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("MasterChief.DotNet.Infrastructure.VerifyCode")]
-[assembly: AssemblyDescription("DotNet VerifyCode")]
+[assembly: AssemblyDescription("DotNet 支持Asp.Net Mvc以及WebForm的验证码功能实现")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MasterChief")]
+[assembly: AssemblyProduct("MasterChief.DotNet.Infrastructure.VerifyCode")]
 [assembly: AssemblyCopyright("Copyright © YanZhiwei 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
--- MasterChief.DotNet4.5.Utilities/Properties/AssemblyInfo.cs
+++ MasterChief.DotNet4.5.Utilities/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("MasterChief.DotNet4.5.Utilities")]
-[assembly: AssemblyDescription("基于.NET Framework4.5 开发类库")]
+[assembly: AssemblyDescription("DotNet .NETFramework4.5 开发类库")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MasterChief")]
+[assembly: AssemblyProduct("MasterChief.DotNet4.5.Utilities")]
 [assembly: AssemblyCopyright("Copyright © YanZhiwei 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
