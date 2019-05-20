afe1e992cb02e818197541cba6fda082c1be9a06
YanZhiwei <Yan.Zhiwei@outlook.com>
2019-05-20 09:43:09 +0800

    增加单元测试；

---
+++ MasterChief.DotNet4.UtilitiesTests/Common/ServiceHelperTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MasterChief.DotNet4.Utilities.Common.Tests
+{
+    [TestClass]
+    public class ServiceHelperTests
+    {
+        [TestMethod]
+        public void IsExistedTest()
+        {
+            var actual = ServiceHelper.IsExisted("PeerDistSvc");
+            Assert.IsTrue(actual);
+
+            actual = ServiceHelper.IsExisted("PeerDistSvc2");
+            Assert.IsFalse(actual);
+        }
+    }
+}
