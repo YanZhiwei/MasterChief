1055cc20484fc3e0428d6d49ea0c799517a1c3fa
YanZhiwei <Yan.Zhiwei@outlook.com>
2019-05-23 09:49:21 +0800

    fix

--- MasterChief.DotNet4.Utilities/Core/ShortcutLink.cs
+++ MasterChief.DotNet4.Utilities/Core/ShortcutLink.cs
@@ -11,24 +11,18 @@ namespace MasterChief.DotNet4.Utilities.Core
     /// </summary>
     public sealed class ShortcutLink
     {
+        #region Methods
+
         /// <summary>
-        ///     创建用户桌面快捷方式
+        ///     检查快捷方式是否已经存在
         /// </summary>
-        /// <param name="desktopPath">存储桌面上的文件对象的目录</param>
+        /// <param name="shortcutFolder">快捷方式文件夹</param>
         /// <param name="name">快捷方式名称</param>
-        /// <param name="programPath">程序路径</param>
-        /// <param name="description">快捷方式描述</param>
-        public static void CreatUserDesktop(string desktopPath, string name, string programPath, string description)
+        /// <returns>是否存在</returns>
+        public static bool CheckExist(string shortcutFolder, string name)
         {
-            ValidateOperator.Begin().NotNullOrEmpty(name, "快捷方式名称")
-                .CheckFileExists(programPath)
-                .CheckDirectoryExist(desktopPath)
-                .NotNullOrEmpty(description, "快捷方式描述");
-            var link = (IShellLink) new ShellLink();
-            link.SetDescription(description);
-            link.SetPath(programPath);
-            var file = link as IPersistFile;
-            file.Save(Path.Combine(desktopPath, $"{name}.lnk"), false);
+            var shortcutLink = Path.Combine(shortcutFolder, $"{name}.lnk");
+            return File.Exists(shortcutLink);
         }
 
         /// <summary>
@@ -37,10 +31,50 @@ namespace MasterChief.DotNet4.Utilities.Core
         /// <param name="name">快捷方式名称</param>
         /// <param name="programPath">程序路径</param>
         /// <param name="description">快捷方式描述</param>
-        public static void CreatCurrentUserDesktop(string name, string programPath, string description)
+        public static string CreatCurUserDesktop(string name, string programPath, string description)
         {
-            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            CreatUserDesktop(desktopPath, name, programPath, description);
+            var desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            return Create(desktopFolder, name, programPath, description);
         }
+
+        /// <summary>
+        ///     创建当前用户开机启动项
+        /// </summary>
+        /// <param name="name">快捷方式名称</param>
+        /// <param name="programPath">程序路径</param>
+        /// <param name="description">快捷方式描述</param>
+        /// <returns></returns>
+        public static string CreatCurUserStartup(string name, string programPath, string description)
+        {
+            var startupFolder = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            return Create(startupFolder, name, programPath, description);
+        }
+
+        /// <summary>
+        ///     创建快捷方式
+        /// </summary>
+        /// <param name="shortcutFolder">快捷方式文件夹</param>
+        /// <param name="name">快捷方式名称</param>
+        /// <param name="programPath">程序路径</param>
+        /// <param name="description">快捷方式描述</param>
+        public static string Create(string shortcutFolder, string name, string programPath, string description)
+        {
+            ValidateOperator.Begin().NotNullOrEmpty(name, "快捷方式名称")
+                .CheckFileExists(programPath)
+                .CheckDirectoryExist(shortcutFolder)
+                .NotNullOrEmpty(description, "快捷方式描述");
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            var link = (IShellLink) new ShellLink();
+            link.SetDescription(description);
+            link.SetPath(programPath);
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            var file = link as IPersistFile;
+            var shortcutLink = Path.Combine(shortcutFolder, $"{name}.lnk");
+            // ReSharper disable once PossibleNullReferenceException
+            file.Save(shortcutLink, false);
+            return shortcutLink;
+        }
+
+        #endregion Methods
     }
 }
