16cdcbc4f270f3e31d93bfab4127e974046eb41c
YanZhiwei <Yan.Zhiwei@outlook.com>
2019-06-06 17:13:13 +0800

    coding

--- MasterChief.DotNet4.Utilities/Common/XMLHelper.cs
+++ MasterChief.DotNet4.Utilities/Common/XMLHelper.cs
@@ -1,49 +1,40 @@
-﻿namespace MasterChief.DotNet4.Utilities.Common
-{
-    using System.Data;
-    using System.IO;
-    using System.Text;
-    using System.Xml;
+﻿using System.Data;
+using System.Xml;
+using System.Xml.Linq;
 
+namespace MasterChief.DotNet4.Utilities.Common
+{
     /// <summary>
-    /// XML 辅助类
+    ///     XML 辅助类
     /// </summary>
     public static class XmlHelper
     {
         #region Methods
 
         /// <summary>
-        /// 格式化xml内容显示
+        ///     格式化xml内容显示
         /// </summary>
-        /// <param name="xmlString">xml内容</param>
-        /// <param name="encoding">encode编码</param>
+        /// <param name="xmlText">xml内容</param>
         /// <returns>string</returns>
-        public static string FormatXml(string xmlString, Encoding encoding)
+        public static string FormatXml(string xmlText)
         {
-            using (MemoryStream stream = new MemoryStream())
-            {
-                using (XmlTextWriter writer = new XmlTextWriter(stream, null))
-                {
-                    XmlDocument xmlDocument = new XmlDocument();
-                    writer.Formatting = Formatting.Indented;
-                    xmlDocument.LoadXml(xmlString);
-                    xmlDocument.WriteTo(writer);
-                    return encoding.GetString(stream.ToArray());
-                }
-            }
+            if (string.IsNullOrEmpty(xmlText)) return xmlText;
+            xmlText = xmlText.Trim();
+            var xDocument = XDocument.Parse(xmlText);
+            return xDocument.ToString().Trim();
         }
 
         /// <summary>
-        /// 将XML文件读取返回成DataSet
+        ///     将XML文件读取返回成DataSet
         /// </summary>
         /// <param name="xmlFilePath">xml路径</param>
         /// <returns>返回DataSet，若发生异常则返回NULL</returns>
         public static DataSet ParseXmlFile(string xmlFilePath)
         {
-            XmlDocument xmlDocument = new XmlDocument();
+            var xmlDocument = new XmlDocument();
             xmlDocument.Load(xmlFilePath);
-            XmlNodeReader nodeReader = new XmlNodeReader(xmlDocument);
-            DataSet dataSet = new DataSet();
+            var nodeReader = new XmlNodeReader(xmlDocument);
+            var dataSet = new DataSet();
             dataSet.ReadXml(nodeReader);
             return dataSet;
         }
