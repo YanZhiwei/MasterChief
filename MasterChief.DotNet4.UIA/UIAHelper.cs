6ae0613bb1ac57f08c0847c7e47ffefcf8d87ef2
YanZhiwei <Yan.Zhiwei@outlook.com>
2019-06-17 11:09:53 +0800

    coding

---
+++ MasterChief.DotNet4.UIA/UIAHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Automation;
+
+namespace MasterChief.DotNet4.UIA
+{
+    /// <summary>
+    /// UI Automation 辅助类
+    /// </summary>
+    public sealed class UIAHelper
+    {
+        public static AutomationElement FindElementById(AutomationElement parentElement, string automationId)
+        {
+            var findElement = parentElement.FindFirst(TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, automationId));
+            return findElement;
+        }
+    }
+}
