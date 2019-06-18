21dc1217227831cd0004e7a150b0280b2a8a38c3
YanZhiwei <Yan.Zhiwei@outlook.com>
2019-06-18 18:49:21 +0800

    coding

--- MasterChief.DotNet4.UIA/UIACombox.cs
+++ MasterChief.DotNet4.UIA/UIACombox.cs
@@ -10,16 +10,19 @@ namespace MasterChief.DotNet4.UIA
     public static class UIACombox
     {
         /// <summary>
-        /// 设置选中项
+        ///     设置选中项
         /// </summary>
         /// <param name="comboBox">AutomationElement</param>
         /// <param name="item">需要选中的文本</param>
-        /// <exception cref="ArgumentException">Element with AutomationId '{comboBox.Current.AutomationId}' and Name '{comboBox.Current.Name}' ControlType is not ComboBox.</exception>
+        /// <exception cref="ArgumentException">
+        ///     Element with AutomationId '{comboBox.Current.AutomationId}' and Name
+        ///     '{comboBox.Current.Name}' ControlType is not ComboBox.
+        /// </exception>
         public static void SetSelectedItem(this AutomationElement comboBox, string item)
         {
             if (comboBox.Current.ControlType != ControlType.ComboBox)
                 throw new ArgumentException(
-                    $"Element with AutomationId '{comboBox.Current.AutomationId}' and Name '{comboBox.Current.Name}' ControlType is not ComboBox.");
+                    $"AutomationId '{comboBox.Current.AutomationId}' and Name '{comboBox.Current.Name}' 控件类型不是 ComboBox.");
             var automationPatternFromElement =
                 GetSpecifiedPattern(comboBox, "ExpandCollapsePatternIdentifiers.Pattern");
 
--- MasterChief.DotNet4.UIA/UIAHelper.cs
+++ MasterChief.DotNet4.UIA/UIAHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Automation;
+﻿using System;
+using System.Windows.Automation;
 
 namespace MasterChief.DotNet4.UIA
 {
@@ -59,6 +60,91 @@ namespace MasterChief.DotNet4.UIA
             var tarFindElement = parentElement.FindAll(TreeScope.Descendants,
                 new PropertyCondition(AutomationElement.ControlTypeProperty, type));
             return tarFindElement;
+        }
+
+        /// <summary>
+        ///     获取ExpandCollapsePattern
+        /// </summary>
+        /// <param name="element">AutomationElement</param>
+        /// <returns>ExpandCollapsePattern</returns>
+        /// <exception cref="Exception">
+        ///     AutomationId '{element.Current.AutomationId}' and Name '{element.Current.Name}' 不支持
+        ///     ExpandCollapsePattern.
+        /// </exception>
+        public static ExpandCollapsePattern GetExpandCollapsePattern(this AutomationElement element)
+        {
+            if (!element.TryGetCurrentPattern(ExpandCollapsePattern.Pattern, out var currentPattern))
+                throw new Exception(
+                    $"AutomationId '{element.Current.AutomationId}' and Name '{element.Current.Name}' 不支持 ExpandCollapsePattern.");
+            return currentPattern as ExpandCollapsePattern;
+        }
+
+        /// <summary>
+        ///     获取SelectionItemPattern
+        /// </summary>
+        /// <param name="element">AutomationElement</param>
+        /// <returns>SelectionItemPattern</returns>
+        /// <exception cref="Exception">
+        ///     AutomationId '{element.Current.AutomationId}' and Name '{element.Current.Name}' 不支持
+        ///     SelectionItemPattern.
+        /// </exception>
+        public static SelectionItemPattern GetSelectionItemPattern(this AutomationElement element)
+        {
+            if (!element.TryGetCurrentPattern(SelectionItemPattern.Pattern, out var currentPattern))
+                throw new Exception(
+                    $"AutomationId '{element.Current.AutomationId}' and Name '{element.Current.Name}' 不支持 SelectionItemPattern.");
+            return currentPattern as SelectionItemPattern;
+        }
+
+        /// <summary>
+        ///     获取InvokePattern
+        /// </summary>
+        /// <param name="element">AutomationElement</param>
+        /// <returns>InvokePattern</returns>
+        /// <exception cref="Exception">
+        ///     AutomationId '{element.Current.AutomationId}' and Name '{element.Current.Name}'
+        ///     不支持InvokePattern.
+        /// </exception>
+        public static InvokePattern GetInvokePattern(this AutomationElement element)
+        {
+            if (!element.TryGetCurrentPattern(InvokePattern.Pattern, out var currentPattern))
+                throw new Exception(
+                    $"AutomationId '{element.Current.AutomationId}' and Name '{element.Current.Name}' 不支持InvokePattern.");
+            return currentPattern as InvokePattern;
+        }
+
+        /// <summary>
+        ///     获取WindowPattern
+        /// </summary>
+        /// <param name="element">AutomationElement</param>
+        /// <returns>WindowPattern</returns>
+        /// <exception cref="Exception">
+        ///     AutomationId '{element.Current.AutomationId}' and Name '{element.Current.Name}'
+        ///     不支持GetWindowPattern.
+        /// </exception>
+        public static WindowPattern GetWindowPattern(this AutomationElement element)
+        {
+            if (!element.TryGetCurrentPattern(WindowPattern.Pattern, out var currentPattern))
+                throw new Exception(
+                    $" AutomationId '{element.Current.AutomationId}' and Name '{element.Current.Name}'  不支持GetWindowPattern.");
+            return currentPattern as WindowPattern;
+        }
+
+        /// <summary>
+        ///     获取TogglePattern
+        /// </summary>
+        /// <param name="element">AutomationElement</param>
+        /// <returns>TogglePattern</returns>
+        /// <exception cref="Exception">
+        ///     AutomationId '{element.Current.AutomationId}' and Name '{element.Current.Name}'
+        ///     不支持TogglePattern.
+        /// </exception>
+        public static TogglePattern GetTogglePattern(this AutomationElement element)
+        {
+            if (!element.TryGetCurrentPattern(TogglePattern.Pattern, out var currentPattern))
+                throw new Exception(
+                    $"AutomationId '{element.Current.AutomationId}' and Name '{element.Current.Name}' 不支持TogglePattern.");
+            return currentPattern as TogglePattern;
         }
     }
 }
