79b7b2bce19dec6d3c3578fd0456cda8d9ef32ef
YanZhiwei <Yan.Zhiwei@outlook.com>
2019-07-05 16:16:30 +0800

    coding

--- MasterChief.DotNet4.Utilities/Common/ProcessHelper.cs
+++ MasterChief.DotNet4.Utilities/Common/ProcessHelper.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 using MasterChief.DotNet4.Utilities.Operator;
 
 namespace MasterChief.DotNet4.Utilities.Common
@@ -11,6 +13,13 @@ namespace MasterChief.DotNet4.Utilities.Common
     /// </summary>
     public static class ProcessHelper
     {
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool OpenProcessToken(IntPtr processHandle, uint desiredAccess, out IntPtr tokenHandle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hwnd);
+
         #region Methods
 
         /// <summary>
@@ -52,6 +61,65 @@ namespace MasterChief.DotNet4.Utilities.Common
             return processes.Count(c =>
                        // ReSharper disable once PossibleNullReferenceException
                        c.MainModule.FileName.StartsWith(workingDirectory, StringComparison.OrdinalIgnoreCase)) > 0;
+        }
+
+        /// <summary>
+        ///     判断程序是否已经运行
+        /// </summary>
+        /// <param name="processPath">程序exe全路径</param>
+        /// <param name="owner">进程拥有者</param>
+        /// <returns>
+        ///     <c>true</c> 是否已经运行 <c>false</c>.
+        /// </returns>
+        public static bool IsRunning(string processPath, string owner)
+        {
+            ValidateOperator.Begin()
+                .NotNullOrEmpty(processPath, "需要运行的程序路径")
+                .NotNullOrEmpty(owner, "进程拥有者")
+                .CheckFileExists(processPath);
+            var result = false;
+            var fileName = Path.GetFileNameWithoutExtension(processPath)?.ToLower();
+            var workingDirectory = Path.GetDirectoryName(processPath);
+            var processes = Process.GetProcessesByName(fileName);
+            if (processes?.Any() ?? false)
+                foreach (var process in processes)
+                {
+                    var procOwner = GetOwner(process);
+                    if (string.IsNullOrEmpty(procOwner)) continue;
+                    if (string.Compare(procOwner, owner, StringComparison.OrdinalIgnoreCase) < 0) continue;
+                    if (process.MainModule.FileName.StartsWith(workingDirectory, StringComparison.OrdinalIgnoreCase))
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+
+            return result;
+        }
+
+        /// <summary>
+        ///     获取进程所有者
+        /// </summary>
+        /// <param name="process">Process</param>
+        /// <returns>进程所有者</returns>
+        private static string GetOwner(Process process)
+        {
+            var processHandle = IntPtr.Zero;
+            try
+            {
+                OpenProcessToken(process.Handle, 8, out processHandle);
+                var wi = new WindowsIdentity(processHandle);
+                var user = wi.Name;
+                return user.Contains(@"\") ? user.Substring(user.IndexOf(@"\", StringComparison.Ordinal) + 1) : user;
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                if (processHandle != IntPtr.Zero) CloseHandle(processHandle);
+            }
         }
 
         #endregion Methods
