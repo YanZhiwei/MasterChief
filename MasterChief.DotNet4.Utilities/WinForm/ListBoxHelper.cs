aea0dca3906a78ae25eb1bf3adeb7cfe514c902d
言志伟 <yanzhiwei@encootech.com>
2019-10-24 18:34:58 +0800

    add AddItemSelected

--- MasterChief.DotNet4.Utilities/WinForm/ListBoxHelper.cs
+++ MasterChief.DotNet4.Utilities/WinForm/ListBoxHelper.cs
@@ -19,11 +19,23 @@ namespace MasterChief.DotNet4.Utilities.WinForm
             var selectedIndex = lsbox.SelectedIndex;
             if (itemCnt > selectedIndex && selectedIndex < itemCnt - 1)
             {
-                object selectedItem = lsbox.SelectedItem;
+                var selectedItem = lsbox.SelectedItem;
                 lsbox.Items.RemoveAt(selectedIndex);
                 lsbox.Items.Insert(selectedIndex + 1, selectedItem);
                 lsbox.SelectedIndex = selectedIndex + 1;
             }
+        }
+
+        /// <summary>
+        ///     添加并选中
+        /// </summary>
+        /// <param name="lsbox">ListBox</param>
+        /// <param name="msg">内容</param>
+        public static void AddItemSelected(this ListBox lsbox, string msg)
+        {
+            if (string.IsNullOrEmpty(msg)) return;
+            lsbox.Items.Add(msg);
+            lsbox.SelectedIndex = lsbox.Items.Count - 1;
         }
 
         /// <summary>
