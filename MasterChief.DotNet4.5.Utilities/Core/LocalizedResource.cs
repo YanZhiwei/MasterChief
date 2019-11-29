1342d6f68bff64f9eaf64e99f23a42dede829503
言志伟 <yanzhiwei@encootech.com>
2019-11-29 15:49:19 +0800

    add LocalizedResource

---
+++ MasterChief.DotNet4.5.Utilities/Core/LocalizedResource.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Resources;
+
+namespace MasterChief.DotNet4._5.Utilities.Core
+{
+    /// <summary>
+    ///     本地化支持
+    /// </summary>
+    public class LocalizedResource
+    {
+        /// <summary>
+        ///     The resource
+        /// </summary>
+        public readonly ResourceManager Resource;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="LocalizedResource" /> class.
+        /// </summary>
+        /// <param name="resourceName">Name of the resource.</param>
+        public LocalizedResource(string resourceName) : this(resourceName, Assembly.GetExecutingAssembly())
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="LocalizedResource" /> class.
+        /// </summary>
+        /// <param name="resourceName">Name of the resource.</param>
+        /// <param name="resourceAssembly">The resource assembly.</param>
+        public LocalizedResource(string resourceName, Assembly resourceAssembly)
+        {
+            Resource = new ResourceManager(resourceName, resourceAssembly);
+        }
+
+        /// <summary>
+        ///     获取本地化文本
+        /// </summary>
+        /// <param name="key">资源Key</param>
+        /// <returns>本地化文本</returns>
+        public virtual string GetString(string key)
+        {
+            if (string.IsNullOrEmpty(key)) return key;
+            var result = Resource.GetString(key, CultureInfo.DefaultThreadCurrentCulture);
+
+            return string.IsNullOrEmpty(result) ? key : result;
+        }
+    }
+}
---
+++ MasterChief.DotNet4.5.UtilitiesTests/Core/LocalizedResourceTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using MasterChief.DotNet4._5.Utilities.Core;
+using MasterChief.DotNet4._5.UtilitiesTests.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MasterChief.DotNet4._5.UtilitiesTests.Core
+{
+    [TestClass]
+    public class LocalizedResourceTests
+    {
+        private LocalizedResource _localizedResource;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _localizedResource = new XfLocalizedResource();
+        }
+
+        [TestMethod]
+        public void GetStringTest()
+        {
+            var actual = _localizedResource.GetString("Name");
+
+            Assert.AreEqual("姓名", actual);
+        }
+    }
+
+    public sealed class XfLocalizedResource : LocalizedResource
+    {
+        public XfLocalizedResource() : base(XfResource.ResourceManager.BaseName, Assembly.GetCallingAssembly())
+        {
+        }
+    }
+}
